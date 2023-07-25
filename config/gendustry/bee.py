11a04eae171c9c557b0d43ab1082f8c1930066b1
prosbloom225 <prosbloom@gmail.com>
2023-07-25 09:31:05 -0500

    adding combs script

--- config/gendustry/bee.py
+++ config/gendustry/bee.py
@@ -4,15 +4,18 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader("templates"))
 bee_template = env.get_template("bee.templ")
 lang_template = env.get_template("lang.templ")
+comb_template = env.get_template("comb.templ")
 
 bees = json.loads(open("bees.json").read())["bees"]
 print(bees)
 bee_output = bee_template.render(bees=bees)
 lang_output = lang_template.render(bees=bees)
+comb_output = comb_template.render(bees=bees)
 
 
 print(bee_output)
 print(lang_output)
+print(comb_output)
 
 bees = open("./custom_bees.cfg", "w")
 bees.write(bee_output)
@@ -21,6 +24,10 @@ bees.close()
 lang = open("./custom_bees.en_US.lang", "w")
 lang.write(lang_output)
 lang.close()
+
+comb = open("../../scripts/comb.zs", "w")
+comb.write(comb_output)
+comb.close()
 
 
 
